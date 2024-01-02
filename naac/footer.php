4ec43c0d8489e5e0869a447024c2594b3696b756
Guntuku Chinmay <153702979+ChinmayGuntuku@users.noreply.github.com>
2024-01-02 20:06:29 +0530

    Add files via upload

---
+++ naac/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer-main">
+        <div class="copy">
+            Copyright &copy; 2023 All rights reserved.<br><a href="https://www.adityatekkali.edu.in/">AITAM</a>
+        </div>
+        <br>
+        <div class="credits">
+            <i>
+                Developed by<div class="names">D. Sourish, G. Ramarohit & G. Chinmay (CSM AR23)</div>
+                Under the esteemed guidance of<div class="names">K.V. Chandra Sekhar</div> Ass. Pro. CSM & CSD
+            </i>
+        </div>
+    </div>
+</footer>
